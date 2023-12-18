59b71e32651f8acf8dda2036d11d0a4136e4c9ac
ekatwikz <ekatwikz@gmail.com>
2023-12-18 04:23:51 +0100

    triangl

--- kekw2.cpp
+++ kekw2.cpp
@@ -1,13 +1,182 @@
 #define GLFW_INCLUDE_NONE
 
-#include "./include/glad/glad.h"
+#include <iostream>
 #include <GLFW/glfw3.h>
 
-int main (int argc, char** argv) {
+#include "./include/glad/glad.h"
+
+// callbacks
+void error_callback(int error, const char* description) {
+    fprintf(stderr, "Oops![0x%08X]: %s\n", error, description);
+    glfwTerminate();
+    exit(EXIT_FAILURE);
+}
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+    (void)window;
+    glViewport(0, 0, width, height);
+}
+void key_handler(GLFWwindow* window, int key, int scancode, int action, int mods) {
+    (void)scancode;
+    (void)mods;
+
+    printf("Key: %d Action: %d\n", key, action);
+
+    // close if Esc pressed
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, GLFW_TRUE);
+    }
+}
+
+// shader sources
+// these raw strings are not a solution. TODO: move them to a new file.
+const char* vertexShaderSource = R"(
+    #version 330 core
+    layout (location = 0) in vec3 aPos;
+    void main() {
+        gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
+    }
+)";
+const char* fragmentShaderSource = R"(
+    #version 330 core
+    out vec4 FragColor;
+    void main() {
+        FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);
+    };
+)";
+
+constexpr uint SCR_WIDTH = 800;
+constexpr uint SCR_HEIGHT = 600;
+int main () {
+    // set the error callback function for glfw stuff
+    glfwSetErrorCallback(error_callback);
+
+    // init glfw
     glfwInit();
+
+    // hint that we'll use OpenGL 3.3 core? not sure exactly
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-    return 0;
+#ifdef __APPLE__
+    // lole
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+
+    // create a window and context, check for errors,
+    // make this window current
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "IM GLing LESSGOOO", NULL, NULL);
+    if (window == NULL) {
+        std::cerr << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return 1;
+    }
+    glfwMakeContextCurrent(window);
+
+    // init the glad loader or something, not sure
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cerr << "Failed to initialize GLAD" << std::endl;
+        return 1;
+    }
+
+    // set the viewport dimensions?
+    glViewport(0, 0, SCR_WIDTH, SCR_HEIGHT);
+
+    // set callbacks for window resizes and keystrokes
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    glfwSetKeyCallback(window, key_handler);
+
+    // setup the array for the vertex buffer object
+    float vertices[] = {
+        -0.5f, -0.5f, 0.0f,
+        0.5f, -0.5f, 0.0f,
+        0.0f,  0.5f, 0.0f
+    };
+
+
+    // setup and bind vertex array object
+    uint VAO;
+    glGenVertexArrays(1, &VAO);
+
+    // setup vertex buffer object and vertex array objects
+    // with our vertices
+    uint VBO;
+    glGenBuffers(1, &VBO);
+    glBindVertexArray(VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    // define the location and format of the vertex attributes,
+    // index=0, b/c we said location=0
+    // 3 b/c 3 values,
+    // GL_FALSE b/c we don't need normalization,
+    // 3*floatsize is stride,
+    // first coord is at [0]
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+
+    // enable the vertex attrib array?
+    glEnableVertexAttribArray(0);
+
+    // compiler vertex shader
+    uint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+
+    // check if vertex shader compilation errored
+    int success;
+    char infoLog[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if(!success) {
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        std::cerr << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+
+    // setup fragment shader
+    uint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+
+    // check if fragment shader compilation errored
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+    if(!success) {
+        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+        std::cerr << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+
+    // create shader program and link the shaders to it
+    uint shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    // check if program linking succeeded
+    glGetShaderiv(shaderProgram, GL_LINK_STATUS, &success);
+    if(!success) {
+        glGetShaderInfoLog(shaderProgram, 512, NULL, infoLog);
+        std::cerr << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+    }
+
+    // le shaders ain't needed any more now
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    // render loop
+    while(!glfwWindowShouldClose(window)) {
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // install the shader program and draw stuffs
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        // glBindVertexArray(0); // no need to unbind it every time
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    // cleanup a little and exit
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteProgram(shaderProgram);
+    glfwTerminate();
 }
