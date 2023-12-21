ae6bf9504017665ab36c76319ea5bdc84c35974f
ekatwikz <ekatwikz@gmail.com>
2023-12-21 21:47:04 +0100

    frag shader: nit

--- shaders/fragment.glsl
+++ shaders/fragment.glsl
@@ -7,4 +7,4 @@ in vec3 triangleColorOutput;
 void main() {
 	vec3 sinThingy = sin(glfwTime * vec3(4, 4.5, 5)) / 2 + 0.5;
 	FragColor = vec4(triangleColorOutput * pow(sinThingy, vec3(0.7)), 1.0);
-};
+}
